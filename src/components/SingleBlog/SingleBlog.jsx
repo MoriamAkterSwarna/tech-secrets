a425392852583f224d92c4e3a08f9b5ebd9d8aef
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-03-30 23:03:24 +0600

    blog section updated

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -8,25 +8,25 @@ const SingleBlog = ({blog}) => {
     return (
         <div>
             <div className="card card-compact bg-base-100 shadow-2xl">
-                <figure><img src={cover} alt="Programming" /></figure>
-                <div className="card-body">
+                <figure className='mt-4'><img style={{height:"400px"}} src={cover} alt="Programming" /></figure>
+                <div className="card-body ml-10">
                     <div className='flex justify-between'>
                         <div className='flex'>
-                            <img src={img} alt="Author Image" className='mr-4'/>
+                            <img src={img} alt="Author Image" className='mr-4 w-12 rounded-full'/>
                             <div>
                                 <h5 className='text-xl'>{authorName}</h5>
                                 <p>{published}</p>
                             </div>
                             
                         </div>
-                        <div className='mr-10'>
+                        <div className='mr-12'>
                                 <p>{time} min read <FontAwesomeIcon icon={faBookmark} /></p>
                             </div>
                     </div>
                     <h2 className="card-title text-2xl font-bold">{title}</h2>
                     <p>{kind}</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Mark as Read</button>
+                    <div className="card-actions">
+                    <button className="btn bg-fuchsia-500">Mark as Read</button>
                     </div>
                 </div>
                 </div>
